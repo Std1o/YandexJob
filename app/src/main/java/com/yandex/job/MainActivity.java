1d63225bc9c0de7ea0c86984af219fa0e560a81d
Std1o <kwork-stdio@mail.ru>
2020-01-16 21:16:38 +0300

    0.0.2

--- app/src/main/java/com/yandex/job/MainActivity.java
+++ app/src/main/java/com/yandex/job/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yandex.job;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, LoggedOutActivity.class));
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
