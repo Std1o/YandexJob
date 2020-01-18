db85c843704b6046d1cc99b9ea2e8f2525b89150
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-18 16:39:52 +0300

    0.1.2

--- app/src/main/java/com/yandex/job/Registration4Activity.java
+++ app/src/main/java/com/yandex/job/Registration4Activity.java
@@ -3,12 +3,21 @@ package com.yandex.job;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class Registration4Activity extends AppCompatActivity {
+    
+    TextView tvConsent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration4);
+        
+        tvConsent = findViewById(R.id.tvConsent);
+        tvConsent.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
