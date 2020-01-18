e1957c8d96a2d6a2f6b60c0acb77c3a0c603f551
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-18 12:16:59 +0300

    0.0.6

--- app/src/main/java/com/yandex/job/LoggedOutActivity.java
+++ app/src/main/java/com/yandex/job/LoggedOutActivity.java
@@ -3,25 +3,12 @@ package com.yandex.job;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.text.Html;
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
 
 public class LoggedOutActivity extends AppCompatActivity {
 
@@ -37,7 +24,7 @@ public class LoggedOutActivity extends AppCompatActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnReg:
-                startActivity(new Intent(this, RegistrationActivity.class));
+                startActivity(new Intent(this, Registration1Activity.class));
                 break;
             case R.id.btnAuth:
                 break;
---
+++ app/src/main/java/com/yandex/job/Registration1Activity.java
@@ -0,0 +1,22 @@
+package com.yandex.job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+
+public class Registration1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration1);
+        String title = "Регистрация: шаг №1";
+        SpannableString s = new SpannableString(title);
+        s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getSupportActionBar().setTitle(s);
+    }
+}
