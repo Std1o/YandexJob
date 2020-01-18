040580551f11d7034764a5efce626af2421e50f3
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-18 14:12:03 +0300

    Update Registration2Activity.java

--- app/src/main/java/com/yandex/job/Registration2Activity.java
+++ app/src/main/java/com/yandex/job/Registration2Activity.java
@@ -2,7 +2,11 @@ package com.yandex.job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 
 public class Registration2Activity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class Registration2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration2);
+        String title = "Регистрация: шаг №2";
+        SpannableString s = new SpannableString(title);
+        s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getSupportActionBar().setTitle(s);
     }
 }
