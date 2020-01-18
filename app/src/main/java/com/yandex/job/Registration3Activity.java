a0a0ddd2562acb1202981f9d4bb66d51c631aff1
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-18 15:59:42 +0300

    0.1.0

--- app/src/main/java/com/yandex/job/Registration3Activity.java
+++ app/src/main/java/com/yandex/job/Registration3Activity.java
@@ -2,27 +2,21 @@ package com.yandex.job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.view.View;
 
-public class Registration2Activity extends AppCompatActivity {
+public class Registration3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration2);
-        String title = "Регистрация: шаг №2";
+        setContentView(R.layout.activity_registration3);
+        String title = "Регистрация: шаг №3";
         SpannableString s = new SpannableString(title);
         s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         getSupportActionBar().setTitle(s);
-    }
-
-    public void onClick(View v) {
-        startActivity(new Intent(this, Registration3Activity.class));
     }
 }
