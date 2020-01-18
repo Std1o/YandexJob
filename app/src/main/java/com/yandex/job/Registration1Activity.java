c92863f2f909112154867a35753358b669314f18
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-18 13:59:07 +0300

    0.0.9

--- app/src/main/java/com/yandex/job/Registration1Activity.java
+++ app/src/main/java/com/yandex/job/Registration1Activity.java
@@ -2,11 +2,13 @@ package com.yandex.job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
 
 public class Registration1Activity extends AppCompatActivity {
 
@@ -18,5 +20,15 @@ public class Registration1Activity extends AppCompatActivity {
         SpannableString s = new SpannableString(title);
         s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         getSupportActionBar().setTitle(s);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnUpload:
+                break;
+            case R.id.btnContinue:
+                startActivity(new Intent(this, Registration2Activity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/yandex/job/Registration2Activity.java
@@ -0,0 +1,14 @@
+package com.yandex.job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration2);
+    }
+}
