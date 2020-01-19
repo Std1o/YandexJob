43af523afa7ed1c4403414fe123110e91ba9ef57
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-19 22:53:10 +0300

    Update Registration2Activity.java

--- app/src/main/java/com/yandex/job/Registration2Activity.java
+++ app/src/main/java/com/yandex/job/Registration2Activity.java
@@ -9,8 +9,11 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.EditText;
 
 public class Registration2Activity extends AppCompatActivity {
+
+    EditText etBrand, etModel, etTSType, etColor, etReleaseYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,27 @@ public class Registration2Activity extends AppCompatActivity {
         SpannableString s = new SpannableString(title);
         s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         getSupportActionBar().setTitle(s);
+        initViews();
     }
 
     public void onClick(View v) {
+        getDataFromEt();
         startActivity(new Intent(this, Registration3Activity.class));
+    }
+
+    private void initViews() {
+        etBrand = findViewById(R.id.etBrand);
+        etModel = findViewById(R.id.etModel);
+        etTSType = findViewById(R.id.etTSType);
+        etColor = findViewById(R.id.etColor);
+        etReleaseYear = findViewById(R.id.etReleaseYear);
+    }
+
+    private void getDataFromEt() {
+        RegistrationData.brand = etBrand.getText().toString();
+        RegistrationData.model = etModel.getText().toString();
+        RegistrationData.TSType = etTSType.getText().toString();
+        RegistrationData.color = etColor.getText().toString();
+        RegistrationData.year = etReleaseYear.getText().toString();
     }
 }
