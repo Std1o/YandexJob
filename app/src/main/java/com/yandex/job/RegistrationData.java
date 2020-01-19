939cb80f0c160448c8045a4f55f14b7dbbccd367
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-19 22:33:54 +0300

    Create RegistrationData.java

---
+++ app/src/main/java/com/yandex/job/RegistrationData.java
@@ -0,0 +1,18 @@
+package com.yandex.job;
+
+public class RegistrationData {
+
+    public static String name;
+    public static String lastName;
+    public static String middleName;
+    public static String birthDate;
+    public static String phone;
+    public static String brand;
+    public static String model;
+    public static String TSType;
+    public static String color;
+    public static String year;
+    public static String login;
+    public static String password;
+    public static String card;
+}
