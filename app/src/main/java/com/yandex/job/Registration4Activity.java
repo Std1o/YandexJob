b5bb898bf8f24a08ce3b051e43dad61738349051
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-19 18:14:24 +0300

    0.2.2

--- app/src/main/java/com/yandex/job/Registration4Activity.java
+++ app/src/main/java/com/yandex/job/Registration4Activity.java
@@ -9,7 +9,25 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registration4Activity extends AppCompatActivity {
     
@@ -27,5 +45,48 @@ public class Registration4Activity extends AppCompatActivity {
         
         tvConsent = findViewById(R.id.tvConsent);
         tvConsent.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public void onClick(View v) {
+        request();
+    }
+
+    private void request() {
+
+        Map<String, String> params = new HashMap<>();
+        params.put("command", "addDriver");
+        params.put("famaly", "famaly");
+        params.put("name", "name");
+        params.put("patronymic", "patronymic");
+        params.put("phone", "89101234567");
+        params.put("work", "own");
+        params.put("car_mark", "car_mark");
+        params.put("car_model", "car_model");
+        params.put("car_color", "car_color");
+        params.put("car_type", "car_type");
+        params.put("car_year", "2010");
+        params.put("login", "login");
+        params.put("pass", "pass");
+        System.out.println(params);
+
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, "http://some-company.svkcom.ru/api/addDriver.php",
+
+                new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Ошибка связи с сервером", Toast.LENGTH_SHORT).show();
+                        Log.e("666", "Autorize - " + error);
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(postRequest);
     }
 }
