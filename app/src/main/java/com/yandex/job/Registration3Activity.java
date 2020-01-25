f1e44fafc9131acca646ee3c32bc122e30687a48
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-25 18:41:41 +0300

    0.2.8

--- app/src/main/java/com/yandex/job/Registration3Activity.java
+++ app/src/main/java/com/yandex/job/Registration3Activity.java
@@ -2,15 +2,26 @@ package com.yandex.job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Base64;
 import android.view.View;
+import android.widget.ImageView;
 
-public class Registration3Activity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
+
+public class Registration3Activity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView ivDL1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,36 @@ public class Registration3Activity extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        startActivity(new Intent(this, Registration4Activity.class));
+        switch (v.getId()) {
+            case R.id.btnContinue:
+                startActivity(new Intent(this, Registration4Activity.class));
+                break;
+            case R.id.btnDL:
+                selectImage();
+                break;
+        }
+    }
+
+    public void selectImage() {
+        Intent intent = new Intent(this, ImageSelectActivity.class);
+        intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_GALLERY, true);//default is true
+        startActivityForResult(intent, 1213);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1213 && resultCode == Activity.RESULT_OK) {
+            String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+            Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            selectedImage.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+            byte[] byteArrayImage = baos.toByteArray();
+            String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+            System.out.println(encodedImage);
+        }
     }
 }
