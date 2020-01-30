7ea84abc4035ea739f001fed163591fda20f35c5
Std1o <37378410+Std1o@users.noreply.github.com>
2020-01-30 17:38:15 +0300

    auth

--- app/src/main/java/com/yandex/job/AuthActivity.java
+++ app/src/main/java/com/yandex/job/AuthActivity.java
@@ -2,13 +2,34 @@ package com.yandex.job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AuthActivity extends AppCompatActivity {
+
+    EditText etLogin, etPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +40,71 @@ public class AuthActivity extends AppCompatActivity {
         SpannableString s = new SpannableString(title);
         s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         getSupportActionBar().setTitle(s);
+        initViews();
+    }
+
+    private void initViews() {
+        etLogin = findViewById(R.id.etLogin);
+        etPass = findViewById(R.id.etPass);
+    }
+
+    public void auth(View view) {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://some-company.svkcom.ru/api" + "/driverRegister.php";
+        Map<String, String> map = new HashMap<>();
+
+        map.put("command", "send_login");
+        map.put("login", etLogin.getText().toString());
+        map.put("pass", etPass.getText().toString());
+        map.put("app", getResources().getString(R.string.app_vr));
+
+        Log.e("666", "Autorize - " + map + "\n" + url);
+
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url,
+
+                new JSONObject(map),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("666", "Autorize - " + response);
+                        try {
+                            String status = response.getString("st");
+                            if (status.equals("-1")) {
+                                Toast.makeText(AuthActivity.this, response.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                            if (status.equals("1")) {
+
+                                Intent intent = new Intent(AuthActivity.this, MainActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("666", "Autorize - " + e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.e("666", "Autorize - " + error);
+                    }
+                }) {
+            @Override
+
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();//
+                // headers.put("Content-Type", "text/html; charset=utf-8");
+                headers.put("User-agent", "Motolife Linux Android");
+                return headers;
+            }
+        };
+        queue.add(postRequest);
+
     }
 }
