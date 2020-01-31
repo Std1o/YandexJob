8dc5fafcd27671fce11ec6cb32d5eb785cbc0be9
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-01 00:00:08 +0300

    Update MainActivity.java

--- app/src/main/java/com/yandex/job/MainActivity.java
+++ app/src/main/java/com/yandex/job/MainActivity.java
@@ -79,6 +79,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
